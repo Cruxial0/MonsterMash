5a7fc7540184d27635f4afcc855f218a4eb1c89f
Cruxial <benjamin@theones.dk>
2022-09-09 22:13:39 +0200

    Moved all scripts to the _Scripts namespace

--- Assets/_Scripts/AC_Ground_RotationController.cs
+++ Assets/_Scripts/AC_Ground_RotationController.cs
@@ -1,65 +1,74 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class AC_Ground_RotationController : MonoBehaviour
+namespace _Scripts
 {
-    public GameObject player;
-    public float rotationRate;
-    private const float MaxRotation = 0.30f;
-    private Keyboard _keyboard;
-    // Start is called before the first frame update
-    void Start()
+    public class AC_Ground_RotationController : MonoBehaviour
     {
-        _keyboard = Keyboard.current;
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (_keyboard.wKey.IsPressed())
+        public GameObject player;
+        public float rotationRate;
+        private const float MaxRotation = 0.30f;
+        private Keyboard _keyboard;
+        // Start is called before the first frame update
+        void Start()
         {
-            if(transform.rotation.x is >= MaxRotation or <= -MaxRotation) return;
-            this.transform.Rotate(new Vector3(rotationRate, 0, 0)  * Time.deltaTime);
-            return;
+            _keyboard = Keyboard.current;
+        
         }
 
-        if (_keyboard.sKey.IsPressed())
+        // Update is called once per frame
+        private void Update()
         {
-            if(transform.rotation.x is >= MaxRotation or <= -MaxRotation) return;
-            this.transform.Rotate(new Vector3(-rotationRate, 0, 0)  * Time.deltaTime);
-            return;
+            if (_keyboard.wKey.IsPressed())
+            {
+                if (transform.rotation.x is < MaxRotation and > -MaxRotation)
+                {
+                    this.transform.Rotate(new Vector3(rotationRate, 0, 0)  * Time.deltaTime);
+                    return;
+                }
+            }
+
+            if (_keyboard.sKey.IsPressed())
+            {
+                if (transform.rotation.x is < MaxRotation and > -MaxRotation)
+                {
+                    this.transform.Rotate(new Vector3(-rotationRate, 0, 0)  * Time.deltaTime);
+                    return;
+                }
+            }
+        
+            if (_keyboard.aKey.IsPressed())
+            {
+                if (transform.rotation.z is < MaxRotation and > -MaxRotation)
+                {
+                    this.transform.Rotate(new Vector3(0, 0, rotationRate)  * Time.deltaTime);
+                    return;
+                }
+            }
+        
+            if (_keyboard.dKey.IsPressed())
+            {
+                if (transform.rotation.z is < MaxRotation and > -MaxRotation)
+                {
+                    this.transform.Rotate(new Vector3(0, 0, -rotationRate)  * Time.deltaTime);
+                    return;
+                }
+            }
+        
+            if(transform.rotation.x == 0 && transform.rotation.z == 0) return;
+        
+            if(transform.rotation.x > 0) 
+                this.transform.Rotate(-rotationRate  * Time.deltaTime, 0, 0);
+            if(transform.rotation.x < 0) 
+                this.transform.Rotate(rotationRate  * Time.deltaTime, 0, 0);
+            if(transform.rotation.z < 0) 
+                this.transform.Rotate(0, 0, rotationRate  * Time.deltaTime);
+            if(transform.rotation.z > 0) 
+                this.transform.Rotate(0, 0, -rotationRate  * Time.deltaTime);
+            if(transform.rotation.y > 0) 
+                this.transform.Rotate(0, -rotationRate  * Time.deltaTime, 0);
+            if(transform.rotation.y < 0) 
+                this.transform.Rotate(0, rotationRate  * Time.deltaTime, 0);
         }
-        
-        if (_keyboard.aKey.IsPressed())
-        {
-            if(transform.rotation.z is >= MaxRotation or <= -MaxRotation) return;
-            this.transform.Rotate(new Vector3(0, 0, rotationRate)  * Time.deltaTime);
-            return;
-        }
-        
-        if (_keyboard.dKey.IsPressed())
-        {
-            if(transform.rotation.z is >= MaxRotation or <= -MaxRotation) return;
-            this.transform.Rotate(new Vector3(0, 0, -rotationRate)  * Time.deltaTime);
-            return;
-        }
-        
-        if(transform.rotation.x == 0 && transform.rotation.z == 0) return;
-        
-        if(transform.rotation.x > 0) 
-            this.transform.Rotate(-rotationRate  * Time.deltaTime, 0, 0);
-        if(transform.rotation.x < 0) 
-            this.transform.Rotate(rotationRate  * Time.deltaTime, 0, 0);
-        if(transform.rotation.z < 0) 
-            this.transform.Rotate(0, 0, rotationRate  * Time.deltaTime);
-        if(transform.rotation.z > 0) 
-            this.transform.Rotate(0, 0, -rotationRate  * Time.deltaTime);
-        if(transform.rotation.y > 0) 
-            this.transform.Rotate(0, -rotationRate  * Time.deltaTime, 0);
-        if(transform.rotation.y < 0) 
-            this.transform.Rotate(0, rotationRate  * Time.deltaTime, 0);
     }
 }
--- Assets/_Scripts/AC_Trigger_BelowGroundChecker.cs
+++ Assets/_Scripts/AC_Trigger_BelowGroundChecker.cs
@@ -1,28 +1,28 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class AC_Trigger_BelowGroundChecker : MonoBehaviour
+namespace _Scripts
 {
-    // Start is called before the first frame update
-    void Start()
+    public class AcTriggerBelowGroundChecker : MonoBehaviour
     {
-        GameObject plane = this.transform.root.gameObject;
+        // Start is called before the first frame update
+        void Start()
+        {
+            GameObject plane = this.transform.root.gameObject;
         
-        var localScale = plane.transform.localScale;
-        print($"X: {localScale.x}, Z: {localScale.z}");
-        this.transform.localScale += new Vector3(localScale.x * 2, 0, localScale.z * 2);
-    }
+            var localScale = plane.transform.localScale;
+            print($"X: {localScale.x}, Z: {localScale.z}");
+            this.transform.localScale += new Vector3(localScale.x * 2, 0, localScale.z * 2);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
-    }
+        }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        collision.collider.gameObject.transform.position += Vector3.up;
+        private void OnCollisionEnter(Collision collision)
+        {
+            collision.collider.gameObject.transform.position += Vector3.up;
+        }
     }
 }
