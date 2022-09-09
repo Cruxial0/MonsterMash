7b4054361f4c469692f3b1db2f6eafa670da0b91
Cruxial <benjamin@theones.dk>
2022-09-09 21:52:00 +0200

    Multiplied all rotation axis by Time.deltaTime

--- Assets/_Scripts/AC_Ground_RotationController.cs
+++ Assets/_Scripts/AC_Ground_RotationController.cs
@@ -7,7 +7,7 @@ public class AC_Ground_RotationController : MonoBehaviour
 {
     public GameObject player;
     public float rotationRate;
-    private const float _maxRotation = 0.30f;
+    private const float MaxRotation = 0.30f;
     private Keyboard _keyboard;
     // Start is called before the first frame update
     void Start()
@@ -21,45 +21,45 @@ public class AC_Ground_RotationController : MonoBehaviour
     {
         if (_keyboard.wKey.IsPressed())
         {
-            if(transform.rotation.x is >= _maxRotation or <= -_maxRotation) return;
-            this.transform.Rotate(new Vector3(rotationRate, 0, 0));
+            if(transform.rotation.x is >= MaxRotation or <= -MaxRotation) return;
+            this.transform.Rotate(new Vector3(rotationRate, 0, 0)  * Time.deltaTime);
             return;
         }
 
         if (_keyboard.sKey.IsPressed())
         {
-            if(transform.rotation.x is >= _maxRotation or <= -_maxRotation) return;
-            this.transform.Rotate(new Vector3(-rotationRate, 0, 0));
+            if(transform.rotation.x is >= MaxRotation or <= -MaxRotation) return;
+            this.transform.Rotate(new Vector3(-rotationRate, 0, 0)  * Time.deltaTime);
             return;
         }
         
         if (_keyboard.aKey.IsPressed())
         {
-            if(transform.rotation.z is >= _maxRotation or <= -_maxRotation) return;
-            this.transform.Rotate(new Vector3(0, 0, rotationRate));
+            if(transform.rotation.z is >= MaxRotation or <= -MaxRotation) return;
+            this.transform.Rotate(new Vector3(0, 0, rotationRate)  * Time.deltaTime);
             return;
         }
         
         if (_keyboard.dKey.IsPressed())
         {
-            if(transform.rotation.z is >= _maxRotation or <= -_maxRotation) return;
-            this.transform.Rotate(new Vector3(0, 0, -rotationRate));
+            if(transform.rotation.z is >= MaxRotation or <= -MaxRotation) return;
+            this.transform.Rotate(new Vector3(0, 0, -rotationRate)  * Time.deltaTime);
             return;
         }
         
         if(transform.rotation.x == 0 && transform.rotation.z == 0) return;
         
         if(transform.rotation.x > 0) 
-            this.transform.Rotate(-rotationRate, 0, 0);
+            this.transform.Rotate(-rotationRate  * Time.deltaTime, 0, 0);
         if(transform.rotation.x < 0) 
-            this.transform.Rotate(rotationRate, 0, 0);
+            this.transform.Rotate(rotationRate  * Time.deltaTime, 0, 0);
         if(transform.rotation.z < 0) 
-            this.transform.Rotate(0, 0, rotationRate);
+            this.transform.Rotate(0, 0, rotationRate  * Time.deltaTime);
         if(transform.rotation.z > 0) 
-            this.transform.Rotate(0, 0, -rotationRate);
+            this.transform.Rotate(0, 0, -rotationRate  * Time.deltaTime);
         if(transform.rotation.y > 0) 
-            this.transform.Rotate(0, -rotationRate, 0);
+            this.transform.Rotate(0, -rotationRate  * Time.deltaTime, 0);
         if(transform.rotation.y < 0) 
-            this.transform.Rotate(0, rotationRate, 0);
+            this.transform.Rotate(0, rotationRate  * Time.deltaTime, 0);
     }
 }
