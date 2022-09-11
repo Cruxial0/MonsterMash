7dc8fec7c9e9fb7537883c715aada29a125d7fc4
Cruxial <benjamin@theones.dk>
2022-09-11 14:39:13 +0200

    Fixed Visual Feedback not instantiating on furniture

--- Assets/_Scripts/MI_Destroy.cs
+++ Assets/_Scripts/MI_Destroy.cs
@@ -7,11 +7,10 @@ public class MI_Destroy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Invoke(nameof(Remove), 1);
-    }
-
-    void Remove()
-    {
+        //Destroy gameObject of which this script is attached to.
         Destroy(gameObject, 1f);
+        
+        //Destroy this script to free up memory.
+        Destroy(this);
     }
 }
