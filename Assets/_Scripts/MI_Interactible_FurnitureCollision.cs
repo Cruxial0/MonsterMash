563717d112e5ca47ac7bb873bd1bd78104eb4cf4
Cruxial <benjamin@theones.dk>
2022-09-11 14:47:27 +0200

    Commented MI_Interactible_FurnitureCollision.cs

--- Assets/_Scripts/MI_Interactible_FurnitureCollision.cs
+++ Assets/_Scripts/MI_Interactible_FurnitureCollision.cs
@@ -4,20 +4,24 @@ namespace _Scripts
 {
     public class MI_Interactible_FurnitureCollision : MonoBehaviour
     {
+        //This is a flag. This flag essentially makes visualFeedback public.
         [SerializeField]
         GameObject visualFeedback;
+        
+        //OnCollisionEnter is called when something collides with Collider and/or Rigidbody
         private void OnCollisionEnter(Collision collision)
         {
+            //if colliding object has the "Player" tag, proceed
             if (collision.gameObject.CompareTag("Player"))
             {
                 //TODO:
                 //Play SFX
                 //Play animation
-
-                print("collided");
+                
+                //Create instance of visualFeedback (probably some kind of animation or particle system)
+                //Also create it with it's original rotation, and on our gameObject's position.
                 var obj = Instantiate(visualFeedback, this.gameObject.transform.position, visualFeedback.transform.rotation);
-                obj.transform.GetChild(0).transform.position = transform.position;
-                //obj.transform.GetChild(0).transform.SetParent(this.transform, false);
+
             }
         }
     }
