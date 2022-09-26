f2921a667f93ea08639e8e3c4295d5f40825554d
Cruxial0 <benjamin@theones.dk>
2022-09-26 12:38:08 +0200

    fixed multi threading issue (hopefully)

--- Assets/_Scripts/Handlers/GameStateManager.cs
+++ Assets/_Scripts/Handlers/GameStateManager.cs
@@ -1,23 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Object = UnityEngine.Object;
 
 namespace _Scripts.Handlers
 {
-    public class GameStateManager
+    public class GameStateManager : UnityEngine.MonoBehaviour
     {
         private GameObject _player;
         private PlayerInteractionHandler _handler;
+        
+        private float delay = 5;
+        private float currTime = 0f;
+        private bool lost = false;
+        
         public GameStateManager(GameObject player, PlayerInteractionHandler handler)
         {
             _player = player;
             _handler = handler;
         }
 
-        public async void Lose()
+        public void Lose()
         {
             PlayerInteractionHandler.SceneObjects.Player.PlayerStates.DestroySelf();
             PlayerInteractionHandler.SceneObjects.UI.Timer.Text.color = Color.red;
@@ -33,41 +40,61 @@ namespace _Scripts.Handlers
             textObjects[0].text = "You lost!";
             textObjects[0].color = Color.red;
             textObjects[1].color = Color.red;
-            
-            if(Application.platform != RuntimePlatform.WebGLPlayer)
-                await Task.Delay(5000);
-            
-            SceneManager.LoadSceneAsync("MenuTest");
-            return;
+
+            lost = true;
+
+            GameObject go = new GameObject();
+            go.AddComponent<GameStateManager>();
         }
         
-        public async void Win()
+        public void Win()
         {
             PlayerInteractionHandler.SceneObjects.Player.PlayerStates.DestroySelf();
             PlayerInteractionHandler.SceneObjects.UI.Timer.Text.color = Color.green;
             PlayerInteractionHandler.SceneObjects.UI.Timer.TimerHandler.StopTimer();
             PlayerInteractionHandler.SceneObjects.Camera.Script.isEnabled = false;
             Object.Instantiate(PlayerInteractionHandler.SceneObjects.Room.BedObject.Script.WinPrefab);
-
-            if(Application.platform != RuntimePlatform.WebGLPlayer)
-                await Task.Delay(5000);
-
-            switch (SceneManager.GetActiveScene().name)
-            {
-                case "Level0":
-                    SceneManager.LoadScene("Level1");
-                    return;
-                case "Level1":
-                    SceneManager.LoadScene("Level2");
-                    return;
-                case "Level2":
-                    SceneManager.LoadScene("Level3");
-                    return;
-            }
             
-            SceneManager.LoadScene("MenuTest");
+            GameObject go = new GameObject();
+            go.AddComponent<GameStateManager>();
+        }
 
-            Debug.Log("You win");
+        private void Awake()
+        {
+            delay = 3f;
+        }
+
+        private void Update()
+        {
+            currTime += Time.deltaTime;
+            print(delay);
+            print(currTime);
+            
+            if (currTime >= delay)
+            {
+                if (lost)
+                {
+                    SceneManager.LoadScene("MenuTest");
+                    return;
+                }
+                
+                switch (SceneManager.GetActiveScene().name)
+                {
+                    case "Level0":
+                        SceneManager.LoadScene("Level1");
+                        return;
+                    case "Level1":
+                        SceneManager.LoadScene("Level2");
+                        return;
+                    case "Level2":
+                        SceneManager.LoadScene("Level3");
+                        return;
+                }
+            
+                SceneManager.LoadScene("MenuTest");
+
+                Debug.Log("You win");
+            }
         }
     }
 }
