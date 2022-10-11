8c4faf12bf8c132b21dd74f3cfc342d16d8ec21f
Cruxial <benjamin@theones.dk>
2022-10-11 16:17:22 +0200

    Implemented the in-game menu screen

---
+++ Assets/_Scripts/GUI/In-Game GUI/BackToMainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BackToMainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Button>().onClick.AddListener(LoadMenu);
+    }
+
+    void LoadMenu()
+    {
+        SceneManager.LoadScene("MenuTest");
+    }
+}
