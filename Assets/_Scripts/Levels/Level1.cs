acedd7542bbfbc7bf01a336bd7b6e1e14447f822
Cruxial0 <benjamin@theones.dk>
2022-10-31 14:18:42 +0100

    Made pickup render on top

--- Assets/_Scripts/Levels/Level1.cs
+++ Assets/_Scripts/Levels/Level1.cs
@@ -3,7 +3,7 @@ using _Scripts.Handlers;
 using _Scripts.Handlers.Events;
 using _Scripts.Interfaces;
 
-namespace DefaultNamespace
+namespace _Scripts.Levels
 {
     public class Level1 : ILevel
     {
@@ -17,8 +17,8 @@ namespace DefaultNamespace
         public StarLevels StarLevels => new StarLevels()
         {
             OneStarRequirement = 0.0,
-            TwoStarRequirement = 0.0,
-            ThreeStarRequirement = 0.0
+            TwoStarRequirement = 20.0,
+            ThreeStarRequirement = 30.0
         };
     }
 }
