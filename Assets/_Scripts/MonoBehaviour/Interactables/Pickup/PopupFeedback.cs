0efcc1bdebde11a8fff169186090624dbdeb4476
Cruxial0 <benjamin@theones.dk>
2022-10-31 10:08:22 +0100

    Added a 1-up effect for pickups

--- Assets/_Scripts/MonoBehaviour/Interactables/Pickup/PopupFeedback.cs
+++ Assets/_Scripts/MonoBehaviour/Interactables/Pickup/PopupFeedback.cs
@@ -5,10 +5,17 @@ namespace _Scripts.MonoBehaviour.Interactables.Pickup
 {
     public class PopupFeedback : UnityEngine.MonoBehaviour
     {
+        private Quaternion iniRotation;
         private void Start()
-        {
+        { 
+            iniRotation = this.transform.rotation;
             this.GetComponent<Animation>().Play("PickupAnimation");
             Destroy(this.gameObject, 10f);
+        }
+
+        private void LateUpdate()
+        {
+            this.transform.rotation = iniRotation;
         }
     }
 }
