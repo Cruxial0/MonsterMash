1b82d1f101153cc489d5d796919735707fbe6cbd
Cruxial0 <benjamin@theones.dk>
2022-11-01 16:09:21 +0100

    Added a simple prototype for the launch trap

---
+++ Assets/_Scripts/MonoBehaviour/Interactables/Traps/LaunchTrap.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using _Scripts.Handlers;
+using _Scripts.Handlers.Interfaces;
+using _Scripts.Handlers.SceneManagers.SceneObjectsHandler;
+using UnityEngine;
+
+public class LaunchTrap : MonoBehaviour, ITrapCollision
+{
+    public ForceDirection trapDirection;
+    public float launchForce;
+    private PlayerInteractionHandler _handler;
+
+    private SceneObjects _sceneObjects;
+
+    private Dictionary<ForceDirection, Vector3> _launchDirection = new()
+    {
+        { ForceDirection.Left, Vector3.left },
+        { ForceDirection.Up, Vector3.up },
+        { ForceDirection.Right, Vector3.right },
+        { ForceDirection.Down, Vector3.down }
+    };
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string TrapName => "LaunchTrap";
+    public GameObject TrapInstance { get; set; }
+    public Animation Animation { get; set; }
+    public void AddInteractionHandlerReference(PlayerInteractionHandler handler)
+    {
+        _handler = handler;
+        _sceneObjects = PlayerInteractionHandler.SceneObjects;
+    }
+
+    public void OnCollision(float playerSpeed)
+    {
+       return;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        print(_launchDirection[trapDirection]);
+        _sceneObjects.Player.Rigidbody
+            .AddForce(_launchDirection[trapDirection] * launchForce, ForceMode.Force);
+        
+        _handler.TrapHandler.Interactibles.First(x => x.Script.TrapName == TrapName)
+            .AddCollisionEntry(new TrapEventArgs(this, other));
+    }
+    
+    public enum ForceDirection
+    {
+        Left,
+        Up,
+        Right,
+        Down,
+    }
+}
