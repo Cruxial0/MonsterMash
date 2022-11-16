f5cc6b455ae81327dbe5b655229f1e99d87915b3
Cruxial0 <benjamin@theones.dk>
2022-11-16 10:13:24 +0100

    Added dark level event

---
+++ Assets/_Scripts/Levels/Level7.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using _Scripts.Handlers;
+using _Scripts.Handlers.Events;
+using _Scripts.Interfaces;
+
+namespace _Scripts.Levels
+{
+    public class Level7 : ILevel
+    {
+        public int LevelID => 7;
+        public Level Level => new Level("DarkLevel")
+        {
+            LevelName = "Dark Level"
+        };
+        public List<IEvent> Events => new() { new LightsOutEvent() };
+        
+        public StarLevels StarLevels => new StarLevels()
+        {
+            OneStarRequirement = 0.0,
+            TwoStarRequirement = 0.0,
+            ThreeStarRequirement = 0.0
+        };
+    }
+}
