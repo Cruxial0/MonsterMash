46db4709de2380a714ac585d56e600879e7e8a46
Cruxial <benjamin@theones.dk>
2022-11-29 19:32:36 +0100

    Fixed issue with trap sounds

--- Assets/_Scripts/Handlers/Events/LightsOutEvent.cs
+++ Assets/_Scripts/Handlers/Events/LightsOutEvent.cs
@@ -10,23 +10,12 @@ namespace _Scripts.Handlers.Events
     /// </summary>
     public class LightsOutEvent : IEvent
     {
-        /// <summary>
-        ///     Inherited from IEvent
-        /// </summary>
         public string EventName => "Lights Out!";
-
         public string Description => "Where did everything go?..";
         public SceneObjects Objects => PlayerInteractionHandler.SceneObjects;
 
         public void ApplyEvent()
         {
-            // //Change light color to black
-            // Objects.Room.LightObject.First().Light.color = Color.black;
-            //
-            // //Change UI Colors to white
-            // Objects.UI.Timer.Text.color = Color.white;
-            // Objects.UI.CollectableCounter.Text.color = Color.white;
-
             // Add the player light component
             var light = Objects.Player.PlayerLight;
             // light.intensity = 2.65f;
--- Assets/_Scripts/Handlers/Events/SecurityCameraEvent.cs
+++ Assets/_Scripts/Handlers/Events/SecurityCameraEvent.cs
@@ -13,11 +13,13 @@ namespace _Scripts.Handlers.Events
         public SceneObjects Objects => PlayerInteractionHandler.SceneObjects;
         private CameraObject _cameraObject;
         private Color c;
+        private static readonly int TintColor = Shader.PropertyToID("_TintColor");
+
         public void ApplyEvent()
         {
             _cameraObject = Objects.Room.EventObjects.First(x => x.GetType() == typeof(CameraObject)) as CameraObject;
             
-            c = _cameraObject.CameraView.MeshRenderer.material.GetColor("_TintColor");
+            c = _cameraObject.CameraView.MeshRenderer.material.GetColor(TintColor);
             
             print(_cameraObject.CameraView.Script.name);
             _cameraObject.CameraView.Script.PlayerInCamera += ScriptOnPlayerInCamera;
@@ -26,12 +28,12 @@ namespace _Scripts.Handlers.Events
 
         private void ScriptOnPlayerExitCamera(object sender)
         {
-            _cameraObject.CameraView.MeshRenderer.material.SetColor("_TintColor", c);
+            _cameraObject.CameraView.MeshRenderer.material.SetColor(TintColor, c);
         }
 
         private void ScriptOnPlayerInCamera(object sender)
         {
-            _cameraObject.CameraView.MeshRenderer.material.SetColor("_TintColor", new Color(255, 0, 0, c.a)); 
+            _cameraObject.CameraView.MeshRenderer.material.SetColor(TintColor, new Color(255, 0, 0, c.a)); 
         }
     }
 }
