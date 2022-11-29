bf0630c5f92bb4e9aab47e7fce7160029ac25ecf
Cruxial <benjamin@theones.dk>
2022-11-29 20:59:59 +0100

    Added new Win screens

--- Assets/_Scripts/GUI/PostLevelScreens/GameStateScreen.cs
+++ Assets/_Scripts/GUI/PostLevelScreens/GameStateScreen.cs
@@ -1,84 +1,45 @@
+using System.Linq;
 using _Scripts.Handlers;
+using _Scripts.Handlers.SceneManagers;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace _Scripts.GUI.PostLevelScreens
 {
     public class GameStateScreen
     {
+        private int starCount = 0;
         public GameObject RestartLevelScreen()
         {
             var restartMenu = PlayerInteractionHandler.SceneObjects.Room.BedObject.Script.WinPrefab;
 
-            //Get text objects
-            var textObjects = restartMenu.GetComponentsInChildren<TextMeshProUGUI>();
-
-            //Format text
-            // textObjects[0].text = "You lost!";
-            // textObjects[0].color = Color.red;
-            //
-            // textObjects[1].text = "Restarting in 5 seconds...";
-            // textObjects[1].color = Color.red;
-            
             restartMenu.transform.GetChild(0).GetComponent<Image>().sprite =
-                restartMenu.GetComponent<AssetContainer>().Sprites[1];
+                restartMenu.GetComponent<AssetContainer>().Sprites[4];
+            
+            restartMenu.transform.GetChild(3).GetComponent<Button>().interactable = false;
 
             return restartMenu;
         }
 
-        public GameObject WinLevelScreen()
+        public GameObject WinLevelScreen(int stars)
         {
+            starCount = stars;
             var winMenu = PlayerInteractionHandler.SceneObjects.Room.BedObject.Script.WinPrefab;
 
-            //Get text objects
-            var textObjects = winMenu.GetComponentsInChildren<TextMeshProUGUI>();
-
-            //Format text
-            // textObjects[0].text = "You win!!";
-            // textObjects[0].color = Color.green;
-            //
-            // textObjects[1].text = "Proceeding to next level...";
-            // textObjects[1].color = Color.green;
-            
             winMenu.transform.GetChild(0).GetComponent<Image>().sprite =
-                winMenu.GetComponent<AssetContainer>().Sprites[0];
+                winMenu.GetComponent<AssetContainer>().Sprites[stars];
+
+            if (stars < 2)
+            {
+                winMenu.transform.GetChild(3).GetComponent<Button>().interactable = false;
+            }
 
             return winMenu;
         }
-
-        public GameObject ReturnToMenuScreenWin()
-        {
-            var winMenu = PlayerInteractionHandler.SceneObjects.Room.BedObject.Script.WinPrefab;
-
-            //Get text objects
-            var textObjects = winMenu.GetComponentsInChildren<TextMeshProUGUI>();
-
-            //Format text
-            textObjects[0].text = "You win!!";
-            textObjects[0].color = Color.green;
-
-            textObjects[1].text = "Returning to the menu in 5 seconds...";
-            textObjects[1].color = Color.green;
-
-            return winMenu;
-        }
-
-        public GameObject ReturnToMenuScreenLose()
-        {
-            var loseMenu = PlayerInteractionHandler.SceneObjects.Room.BedObject.Script.WinPrefab;
-
-            //Get text objects
-            var textObjects = loseMenu.GetComponentsInChildren<TextMeshProUGUI>();
-
-            //Format text
-            textObjects[0].text = "You lose!";
-            textObjects[0].color = Color.red;
-
-            textObjects[1].text = "Returning to the menu in 5 seconds...";
-            textObjects[1].color = Color.red;
-
-            return loseMenu;
-        }
+        
+        
     }
 }
